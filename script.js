b9598a30b15daf4745af03cb6ef7b91386fec343
narkeeta <narkeetadesign@gmail.com>
2018-08-18 19:59:58 -0700

    Forgot how math random worked, fixed that, also opacity shit

--- script.js
+++ script.js
@@ -4,45 +4,48 @@
 
 $(document).ready(function () {
 	function getFlicker() {
-		var rNum = Math.floor((Math.random() * 10) + 8);
+		var rNum = Math.floor((Math.random() * 20) + 79);
 		return rNum;
 	}
 	function getDelay() {
-		var rNum2 = Math.floor((Math.random() * 1500) + 800);
+		var rNum2 = Math.floor((Math.random() * 700) + 800);
 		return rNum2;
 	}
 	function getDelayShort() {
-		var rNum3 = Math.floor((Math.random() * 800) + 300);
+		var rNum3 = Math.floor((Math.random() * 500) + 300);
 		return rNum3;
 	}
 	function runFlicker(selectorClass) {
 		var bTime = getDelay();
 		var bTime2 = getDelayShort();
+		var bOpacity = getFlicker();
 		setTimeout(function () {
-			$(selectorClass + " .blue").removeClass('off');
+			$(selectorClass + " .blue").css('opacity', '.' + bOpacity );
 		}, bTime);
 		setTimeout(function () {
-			$(selectorClass + " .blue").addClass('off');
+			$(selectorClass + " .blue").css('opacity', '.6');
 		}, bTime2);
 		
 		
 		var yTime = getDelay();
 		var yTime2 = getDelayShort();
+		var yOpacity = getFlicker();
 		setTimeout(function () {
-			$(selectorClass + " .yellow").removeClass('off');
+			$(selectorClass + " .yellow").css('opacity', '.' + yOpacity );
 		}, yTime);
 		setTimeout(function () {
-			$(selectorClass + " .yellow").addClass('off');
+			$(selectorClass + " .yellow").css('opacity', '.6');
 		}, yTime2);
 		
 		
 		var rTime = getDelay();
 		var rTime2 = getDelayShort();
+		var rOpacity = getFlicker();
 		setTimeout(function () {
-			$(selectorClass + " .red").removeClass('off');
+			$(selectorClass + " .red").css('opacity', '.' + rOpacity );
 		}, rTime);
 		setTimeout(function () {
-			$(selectorClass + " .red").addClass('off');
+			$(selectorClass + " .red").css('opacity', '.6');
 		}, rTime2);
 	}
 	
